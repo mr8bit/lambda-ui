481b63d2798e11a690ef7fa7f2de301499e4edf5
mr8bit <69kissmysausage@gmail.com>
2018-02-06 02:57:14 +0300

    Добавил методы оплаты + js обработку

--- app/blocks/e-commerce/menu/menu.js
+++ app/blocks/e-commerce/menu/menu.js
@@ -2,8 +2,7 @@ var submenus = [].slice.call(document.getElementsByClassName("menu-submenu"));
 
 submenus.forEach(function (element, index) {
     element.addEventListener("click", function () {
-
-        element.childNodes[element.childNodes.length - 1].classList.toggle("icon-rotate-90")
+        element.childNodes[element.childNodes.length - 1].classList.toggle("icon-rotate-90");
         var target = element.getAttribute('data-target');
         var submenu = document.getElementById(target);
         submenu.classList.toggle("menu--open");
